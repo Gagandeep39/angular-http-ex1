794a3b5b0935d1194fc29feb9a0ee696e0628247
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 22:19:19 +0530

    Created a LoggingInterceptorService

---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,28 @@
+import { tap } from 'rxjs/operators';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpEventType
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log('Logging: Outgoing request');
+    console.log(req.body);
+
+    return next.handle(req).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Logging: Incoming response');
+          console.log(event.body);
+        }
+      })
+    );
+  }
+}
