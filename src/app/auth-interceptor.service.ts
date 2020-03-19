a3f8fa5f144e93d749fc9f5e86bc36180bcc6030
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 21:54:59 +0530

    Added a comment for hint

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -10,6 +10,9 @@ export class AuthInterceptorService implements HttpInterceptor {
     req: import('@angular/common/http').HttpRequest<any>,
     next: import('@angular/common/http').HttpHandler
   ) {
+    // We can enable/disable is for specific URL as follow
+    // if (req.url === 'www.google.com')
+
     console.log('Intercepted');
     return next.handle(req);
   }
