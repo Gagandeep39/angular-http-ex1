636e8461cabb4c833096e7e7dbfb7f2b0b002ae4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 13:01:42 +0530

    Fetech postcalled on start Added a pipe to restructure recieved data Observable data is now explicilty made Post from any

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { Post } from './post.model';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,30 +10,59 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  fetchedPost;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
   // Parameters -> varName: ObjectType/Structure
   onCreatePost(postData: { title: string; content: string }) {
     console.log(postData);
     // Send Http request
     this.http
-      .post(
-        'https://fir-contact-c6ceb.firebaseio.com/post.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log('Response: ' + responseData);
-      }, (error) => {
-        console.log('Error: ' + error);
-      }, () => {
-        console.log('Completed');
-      });
+      .post('https://fir-contact-c6ceb.firebaseio.com/post.json', postData)
+      .subscribe(
+        responseData => {
+          console.log('Response: ' + responseData);
+        },
+        error => {
+          console.log('Error: ' + error);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
   }
 
   onFetchPosts() {
+    this.http
+      .get('https://fir-contact-c6ceb.firebaseio.com/post.json')
+      .pipe(
+        map((responseData: {[key: string]: Post})  => {
+          const responseArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              responseArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return responseArray;
+        })
+      )
+      .subscribe(
+        responseData => {
+          this.fetchedPost = responseData;
+          console.log(responseData);
+        },
+        error => {
+          console.log('Error: ' + error);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
     // Send Http request
   }
 
@@ -39,5 +70,13 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 }
-// Append /post.json to store it in a folder named post in firebase 
-// Http requests wont be sent if we dont subscribe for response 
+// Append /post.json to store it in a folder named post in firebase
+// Http requests wont be sent if we dont subscribe for response
+// MAP is an operator to map something to something else
+// Here we are iterating through response data
+// 1. for every key, we will check if the data has a key
+// 2. then we will push the conent mapped with key directly (Multiple key value pairs are pushed together using '...')
+// 3. To preseve the keyof, we will add it as an additional key value pair
+// {[key: string]: Post}
+// Here key is the root value 
+// Post object is the nested structure 
