a77e6f22bafd97fd48f77875311e20701f5fc2d5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 14:57:11 +0530

    Created a service class to carry logical operations

---
+++ src/app/post.service.ts
@@ -0,0 +1,48 @@
+import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(postData: Post) {
+    console.log(postData);
+    this.http
+      .post<{ lol: string; ll: string }>(
+        'https://fir-contact-c6ceb.firebaseio.com/post.json',
+        postData
+      )
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        },
+        error => {
+          console.log('Error: ' + error);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
+  }
+
+  fetchPost() {
+    return this.http
+      .get<{[key: string]: Post}>('https://fir-contact-c6ceb.firebaseio.com/post.json')
+      .pipe(
+        map((responseData)  => {
+          const responseArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              responseArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return responseArray;
+        })
+      );
+
+  }
+}
